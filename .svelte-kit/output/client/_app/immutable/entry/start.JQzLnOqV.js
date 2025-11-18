eee4ec063968c0b2329e09e143f5ee433720bc26
Julie Schaub <julie.schaub@hotmail.com>
2025-11-18 15:44:16 -0600

    fixed npm run build error

--- .svelte-kit/output/client/_app/immutable/entry/start.JQzLnOqV.js
+++
@@ -1,1 +0,0 @@
-import{l as o,d as r}from"../chunks/6N6VOKC0.js";export{o as load_css,r as start};
--- .svelte-kit/output/server/chunks/internal.js
+++ .svelte-kit/output/server/chunks/internal.js
@@ -2630,7 +2630,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "rj8glx"
+  version_hash: "ur2v98"
 };
 async function get_hooks() {
   let handle;
